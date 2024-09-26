09fa3720c3608a40c2bfd02aa1872d24f5eaf138
unknown <kaustubh.talele2018@gmail.com>
2024-09-26 11:43:10 +0530

    Added Go Language Folder

---
+++ gofile/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello From Krishna With Go in Docker!")
+}
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+    fmt.Println("Server is running on port 7000...")
+    log.Fatal(http.ListenAndServe(":7000", nil))
